f9181435323e80803d16ba977043b1dc8138b75e
Lowell Alleman <lowell@securecoders.com>
2026-03-11 21:24:17 -0400

    Issue ui_restart using a POST.
    The API docs say GET should work too, but it stopped working.  Besides, POST is more REST-like anyway. Generally GET shouldn't have side-effects like restarting things.

--- update_cert.py
+++ update_cert.py
@@ -142,7 +142,8 @@ for cert_id in ui_certificates.keys():
     except Exception:
         print(f"Failed to delete cert {cert_id}, skip")
 
-time.sleep(3)
-
 print("Reloading TrueNAS web UI")
-req_get(f"{API_BASE_URL}/system/general/ui_restart")
+
+# Default wait is 3 seconds
+response = requests.post(f"{API_BASE_URL}/system/general/ui_restart", headers=headers, verify=False)
+response.raise_for_status()
